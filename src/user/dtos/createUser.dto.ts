a6ed181947ff361f06d1df98ecc0a468c30f163b
Samir Dilly <samir9.dilly@gmail.com>
2023-05-02 18:53:02 -0300

    feat: create userControler and DTO

---
+++ src/user/dtos/createUser.dto.ts
@@ -0,0 +1,8 @@
+export interface CreateUserDto {
+    name: string;
+    email: string;
+    phone: string;
+    cpf: string;
+    password: string;
+
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
 
-    @Get()
-    async getAllUsers() {
-        return JSON.stringify({test: 'abc'});
+    @Post()
+    async createUser(
+        @Body() createUser: CreateUserDto
+    ) {
+        return {
+            ...createUser,
+            password: undefined,
+        };
     }
 }
