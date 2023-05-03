1327afd4aca90edb2c3ce811f3aa277f689f9413
Samir Dilly <samir9.dilly@gmail.com>
2023-05-03 12:03:41 -0300

    feat: create userEntity

---
+++ src/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
+import { hash } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>,
+    ){}
+
+    async createUser(CreateUserDto: CreateUserDto) : Promise<UserEntity> {
+
+        const saltOrRounds = 10;
+
+        const passwordHashed = await hash(CreateUserDto.password, saltOrRounds);
+
+        return this.userRepository.save({
+            ...CreateUserDto,
+            password: passwordHashed,
+        })
+    }
+
+    async getAllUser(): Promise<UserEntity[]> {
+        return this.userRepository.find();
+    }
+
+}
