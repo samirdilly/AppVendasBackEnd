9664546e90c3457080eb420dee8ea57befe9298f
Samir Dilly <samir9.dilly@gmail.com>
2023-05-03 14:29:06 -0300

    feat: create insert migrate in project

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -20,6 +20,7 @@ export class UserService {
 
         return this.userRepository.save({
             ...CreateUserDto,
+            typeUser: 1,
             password: passwordHashed,
         })
     }
