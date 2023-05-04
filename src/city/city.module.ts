2b5494e9911a67cb2e9c64d663a508f9bbc10d15
Samir Dilly <samir9.dilly@gmail.com>
2023-05-04 15:31:16 -0300

    feat: create cache function global

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -3,15 +3,17 @@ import { CityController } from './city.controller';
 import { CityService } from './city.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CityEntity } from './entities/city.entity';
-import { CacheModule } from '@nestjs/cache-manager';
+import { CacheModule as CacheModuleNest } from '@nestjs/cache-manager';
+import { CacheModule } from 'src/cache/cache.module';
+
 
 @Module({
-  imports:[
-    CacheModule.register({
-    ttl: 900000000,
+  imports:[ 
+    CacheModuleNest.register({
+    ttl:9000000000,
   }), 
-  TypeOrmModule.forFeature([CityEntity])
-  ],
+  CacheModule,
+  TypeOrmModule.forFeature([CityEntity])],
   controllers: [CityController],
   providers: [CityService]
 })
