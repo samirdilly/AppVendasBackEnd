cfa7c7dcffdaea3aa25d23087f25a3aa9cb72a2e
Samir Dilly <samir9.dilly@gmail.com>
2023-05-08 09:58:15 -0300

    feat: insert validation in addres and city

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -10,6 +10,7 @@ import { CacheModule } from 'src/cache/cache.module';
 @Module({
   imports:[CacheModule,TypeOrmModule.forFeature([CityEntity])],
   controllers: [CityController],
-  providers: [CityService]
+  providers: [CityService],
+  exports: [CityService],
 })
 export class CityModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { UserEntity } from './entities/user.entity';
 import { hash } from 'bcrypt';
@@ -27,6 +27,20 @@ export class UserService {
 
     async getAllUser(): Promise<UserEntity[]> {
         return this.userRepository.find();
+    }
+
+    async findUserById(userId: number): Promise<UserEntity> {
+        const user = await this.userRepository.findOne({
+            where: {
+                id: userId,
+            },
+        });
+
+        if (!user) {
+            throw new NotFoundException('UserId Not Found');
+        }
+
+        return user;
     }
 
 }
